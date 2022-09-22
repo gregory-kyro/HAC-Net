585adada7774d48c5472acbb5c574fdd8853a130
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2022-09-22 08:12:01 -0400

    Update Linear_model.py

--- 3D-CNN/Linear_model.py
+++ 3D-CNN/Linear_model.py
@@ -9,7 +9,6 @@ class Model_Linear(nn.Module):
 		
 		self.fc1 = nn.Linear(2048, 100)
 		torch.nn.init.normal_(self.fc1.weight, 0, 1)
-		self.dropout1 = nn.Dropout(0.0)
 		self.fc1_bn = nn.BatchNorm1d(num_features=100, affine=True, momentum=0.3).train()
 		self.fc2 = nn.Linear(100, 1)
 		torch.nn.init.normal_(self.fc2.weight, 0, 1)
@@ -18,7 +17,6 @@ class Model_Linear(nn.Module):
 	def forward(self, x):
 		fc1_z = self.fc1(x)
 		fc1_y = self.relu(fc1_z)
-		fc1_d = self.dropout1(fc1_y)
-		fc1 = self.fc1_bn(fc1_d) if fc1_d.shape[0]>1 else fc1_d  
+		fc1 = self.fc1_bn(fc1_y) if fc1_y.shape[0]>1 else fc1_y  
 		fc2_z = self.fc2(fc1)
 		return fc2_z, fc1_z
