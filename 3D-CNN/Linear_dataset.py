39a438110141c47cc602996f8b3ec6c36bd25efd
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2022-12-29 14:36:50 -0500

    Update Linear_dataset.py

--- 3D-CNN/Linear_dataset.py
+++ 3D-CNN/Linear_dataset.py
@@ -2,21 +2,24 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
-""" Define a class to contain the data that will be included in the dataloader 
-sent to the Linear model """
+""" Define a class to contain the extracted 3D-CNN features that will be included in the dataloader 
+sent to the fully-connected network """
 
-class Dataset_npy(Dataset):
+class Linear_Dataset(Dataset):
 	def __init__(self, npy_path, feat_dim=22):
-		super(Dataset_npy, self).__init__()
+		super(Linear_Dataset, self).__init__()
 		self.npy_path = npy_path
 		self.input_feat_array = np.load(npy_path, allow_pickle=True)[:,:-1].astype(np.float32)
 		self.input_affinity_array = np.load(npy_path, allow_pickle=True)[:,-1].astype(np.float32)
 		self.data_info_list = []
+		
 	def __len__(self):
 		count = self.input_feat_array.shape[0]
 		return count
+
 	def __getitem__(self, idx):
 		data, affinity = self.input_feat_array[idx], self.input_affinity_array[idx]
+
 		x = torch.tensor(data)
 		y = torch.tensor(np.expand_dims(affinity, axis=0))
 		return x,y
