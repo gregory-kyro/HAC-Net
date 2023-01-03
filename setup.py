cf3206086eb834ee2757410c3308e88417245411
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2023-01-03 16:45:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'HAC-Net',
     packages = ['HAC-Net'],
-    version = '1.3'
+    version = '2.0'
     description = 'a Hybrid Attention-based Convolutional neural Network for highly accurate protein-ligand binding affinity prediction',
     author = 'Gregory W. Kyro, Rafael, I. Brent',
     author_email = 'gregory.kyro@yale.edu, rafi.brent@yale.edu',
     url = 'https://github.com/gregory-kyro/HAC-Net/tree/main/HAC-Net',
     download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.3.tar.gz',
-    keywords = ['HAC-Net', 'v1.3'],
+    keywords = [],
     classifiers = [],
 )
