2fcfface3562ca9660f887df3a9a8c7ceacc0305
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2023-01-05 12:47:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'HACNet',
     packages = ['HACNet'],
-    version = '3',
+    version = '1.0.0',
     description = 'Python package',
     author = 'Gregory W. Kyro',
     author_email = 'gregory.kyro@yale.edu',
-    url = 'https://github.com/gregory-kyro/HAC-Net/tree/main/src',
-    download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v3.0.tar.gz',
+    url = 'https://github.com/gregory-kyro/HAC-Net/tree/main/HACNet',
+    download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.3.tar.gz',
     keywords = [],
     classifiers = []
 )
