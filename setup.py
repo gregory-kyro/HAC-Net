94f8602639e0cb7e7e6b34a61ae6953d7a347966
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2023-01-06 15:08:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Gregory W. Kyro',
     author_email = 'gregory.kyro@yale.edu',
     url = 'https://github.com/gregory-kyro/HAC-Net/tree/main/HACNet',
-    download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.3.tar.gz',
+    download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.3.1.tar.gz',
     keywords = [],
     classifiers = []
 )
