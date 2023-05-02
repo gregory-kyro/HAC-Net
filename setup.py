171193b6968b2ac356c194725a4cc2c9e410da25
Gregory Kyro <98780179+gregory-kyro@users.noreply.github.com>
2023-05-02 13:53:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'HACNet',
     packages = ['HACNet'],
-    version = '1.4.1',
+    version = '1.4.2',
     description = 'HACNet Python package for predicting protein-ligand binding affinity',
     author = 'Gregory W. Kyro',
     author_email = 'gregory.kyro@yale.edu',
     url = 'https://github.com/gregory-kyro/HAC-Net/tree/main/HACNet',
-    download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.4.1.tar.gz',
+    download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.4.2.tar.gz',
     keywords = [],
     classifiers = []
 )
